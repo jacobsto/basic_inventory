c56fe337f25f1580080fc2c8c4d533e4749620f9
jacobsto <jast2016@outlook.com>
2025-11-03 15:43:00 +0000

    Users CSV and Headers

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ from datetime import datetime # Imports datetime class allowing for timestamps
 
 CSV_PATH = "data.csv" #File name for inventory csv
 COLUMNS = ["item_id","item_name","quantity","unit","added_by","date_added"] #Headers for columns
+
+USERS_CSV = "users.csv" #File name for users csv
+USER_COLUMNS = ["username","password","role"] #Headers for columns
