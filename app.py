468d02803741ebac4bb8bbd689a1f30f3f05bbac
jacobsto <jast2016@outlook.com>
2025-11-03 15:43:28 +0000

    Find/Create Inventory CSV

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ COLUMNS = ["item_id","item_name","quantity","unit","added_by","date_added"] #Hea
 
 USERS_CSV = "users.csv" #File name for users csv
 USER_COLUMNS = ["username","password","role"] #Headers for columns
+
+def ensure_csv_ready():
+    if not os.path.exists(CSV_PATH) or os.path.getsize(CSV_PATH) == 0: #Checks if CSV exists
+        with open(CSV_PATH, "w", newline="", encoding="utf-8") as f: #Opens file in write mode
+            csv.writer(f).writerow(COLUMNS) #Writes column headers into the first line
