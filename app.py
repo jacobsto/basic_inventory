3f13beaad4dd757c0c1996d2b348dc0f050be769
jacobsto <jast2016@outlook.com>
2025-11-03 15:42:28 +0000

    Inventory CSV and Headers

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 import csv, os #Load CSV module, Load OS module
 from datetime import datetime # Imports datetime class allowing for timestamps
+
+CSV_PATH = "data.csv" #File name for inventory csv
+COLUMNS = ["item_id","item_name","quantity","unit","added_by","date_added"] #Headers for columns
